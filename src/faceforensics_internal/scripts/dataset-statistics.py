2c39fa6b4e7534047c0d387f45185d655968c1ed
Christoph Clement <christoph.clement@tum.de>
2019-12-22 13:53:27 +0100

    Save dataframe

--- src/faceforensics_internal/scripts/dataset-statistics.py
+++ src/faceforensics_internal/scripts/dataset-statistics.py
@@ -67,12 +67,12 @@ def main(video_root_dir, ff_all_root_dir, compressions, methods):
 
     for videos_sub_dir in videos_data_structure.get_subdirs():
 
-        method, compression, data_type = videos_sub_dir.parts[-3:]
+        method, compression = videos_sub_dir.parts[-3:-1]
 
         key = f"{method}_{compression}"
         videos_frame_counts[key] = 0
 
-        for video_path in sorted(videos_sub_dir.iterdir())[:1]:
+        for video_path in sorted(videos_sub_dir.iterdir()):
             video_cap = cv2.VideoCapture(str(video_path))
             num_frames = int(video_cap.get(cv2.CAP_PROP_FRAME_COUNT))
             videos_frame_counts[key] += num_frames
@@ -83,11 +83,11 @@ def main(video_root_dir, ff_all_root_dir, compressions, methods):
         key = f"{method}_{compression}"
         face_information_frame_counts[key] = 0
 
-        for json_path in sorted(face_information_sub_dir.iterdir())[:1]:
+        for json_path in sorted(face_information_sub_dir.iterdir()):
             with open(str(json_path), "r") as f:
                 json_dict = json.load(f)
 
-            face_information_frame_counts[key] = len(json_dict)
+            face_information_frame_counts[key] += len(json_dict)
 
     for bounding_boxes_sub_dir in bounding_boxes_data_structure.get_subdirs():
         method, compression, data_type = bounding_boxes_sub_dir.parts[-3:]
@@ -95,11 +95,11 @@ def main(video_root_dir, ff_all_root_dir, compressions, methods):
         key = f"{method}_{compression}"
         bounding_boxes_frame_counts[key] = 0
 
-        for json_path in sorted(bounding_boxes_sub_dir.iterdir())[:1]:
+        for json_path in sorted(bounding_boxes_sub_dir.iterdir()):
             with open(str(json_path), "r") as f:
                 json_dict = json.load(f)
 
-            bounding_boxes_frame_counts[key] = len(json_dict)
+            bounding_boxes_frame_counts[key] += len(json_dict)
 
     for face_images_tracked_sub_dir in face_images_tracked_data_structure.get_subdirs():
         method, compression, data_type = face_images_tracked_sub_dir.parts[-3:]
@@ -107,16 +107,34 @@ def main(video_root_dir, ff_all_root_dir, compressions, methods):
         key = f"{method}_{compression}"
         face_images_tracked_frame_counts[key] = 0
 
-        for sub_dir_path in sorted(face_images_tracked_sub_dir.iterdir())[:1]:
-            face_images_tracked_frame_counts[key] = len(list(sub_dir_path.iterdir()))
+        for sub_dir_path in sorted(face_images_tracked_sub_dir.iterdir()):
+            face_images_tracked_frame_counts[key] += len(list(sub_dir_path.iterdir()))
 
-    print(videos_frame_counts)
-    print(face_information_frame_counts)
-    print(bounding_boxes_frame_counts)
-    print(face_images_tracked_frame_counts)
+    df_videos = pd.DataFrame.from_dict(
+        videos_frame_counts, orient="index", columns=["videos"]
+    )
+    df_face_information = pd.DataFrame.from_dict(
+        face_information_frame_counts, orient="index", columns=["face_information"]
+    )
+    df_bounding_boxes = pd.DataFrame.from_dict(
+        bounding_boxes_frame_counts, orient="index", columns=["bounding_boxes"]
+    )
+    df_face_images_tracked = pd.DataFrame.from_dict(
+        face_images_tracked_frame_counts,
+        orient="index",
+        columns=["face_images_tracked"],
+    )
 
-    df = pd.DataFrame.from_dict(videos_frame_counts)
-    print(df)
+    df_frame_counts = (
+        df_videos.join(df_face_information)
+        .join(df_bounding_boxes)
+        .join(df_face_images_tracked)
+    )
+
+    print(df_frame_counts)
+    print(df_frame_counts.describe())
+
+    df_frame_counts.to_csv("frame_counts.csv")
 
 
 if __name__ == "__main__":
